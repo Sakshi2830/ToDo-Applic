f2090c34eaa9b15a9ba669f2567e8f14f46a2a66
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-06-07 01:14:57 +0530

    adding update and delete option

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,29 @@
+import { Button, ListItem, ListItemText } from '@material-ui/core'
+
 import React from 'react'
+import { db } from './firebase-config'
 
 export default function TodoListItems({todo, inprogress, id}) {
+
+    function toggleInProgress() {
+        db.collection("todos").doc(id).update({
+            inprogress: !inprogress,
+        })
+    }
+
+    function toDelete(){
+        db.collection("todos").doc(id).delete();
+    }
+
     return (
-        <div>
-            <p>{todo}</p>
+        <div style={{display: "flex"}}>
+            <ListItem >
+           <ListItemText primary={todo} 
+           secondary={inprogress ? "In Progress" : "Completed"} />
+           </ListItem>
+
+           <Button onClick={toggleInProgress}>{inprogress ? "Done" : "UnDone"}</Button>
+           <Button onClick={toDelete}>❌</Button>
         </div>
     )
 }
